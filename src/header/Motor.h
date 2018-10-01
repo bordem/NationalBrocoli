0b34a676b258311e215ad5e1903d246a2909f11f
bordem <martin.borde19@gmail.com>
2018-10-01 15:08:54 +0200

    Ajout de certain header

--- src/header/Motor.h
+++ src/header/Motor.h
@@ -2,19 +2,19 @@
 
 class Motor{
 
-	enum pos{
-		droite,
-		gauche
-	}
-
 	private:
 		MeEncoderOnBoard motor;
 		const float correction;
+		const char port;
 		pos position;
 
 	public:
+		Motor(char port,pos position);
 		void forward(char speed);
 		void backward(char speed);
 
-
+		enum pos{
+			droite,
+			gauche
+		}
 };
