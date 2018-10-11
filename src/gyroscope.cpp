da523592745f73960c895b88be8cee64552dd0b1
Nicolai Dagestad <nicolai@dagestad.fr>
2018-10-12 00:33:19 +0200

    Gyro

--- src/gyroscope.cpp
+++ src/gyroscope.cpp
@@ -2,14 +2,30 @@
 
 Gyroscope::Gyroscope(uint8_t ports[2], int max_iter):
 sensors{MeGyro(ports[0]),MeGyro(ports[1])},
-gyro_nb{2},
-iterations{max_iter}
-{}
-
-void Gyroscope::rese(){
-	
+gyro_nb(2),
+iterations(max_iter)
+{
+	this->reset();
 }
 
-double Gyroscope::angle(){
-	return 0;
+void Gyroscope::reset(){
+	for ( int i=0; i<gyro_nb; i++){
+		sensors[i].begin();
+	}
+}
+
+double* Gyroscope::angle(){
+
+	double xs=0, ys=0, zs=0;
+	for (int i=0; i<gyro_nb; i++){
+		sensors[i].update();
+		xs+=sensors[i].getAngleX();
+		ys+=sensors[i].getAngleY();
+		zs+=sensors[i].getAngleZ();
+	}
+	double* vals = new double[3]{xs/this->gyro_nb, ys/this->gyro_nb, zs/this->gyro_nb};
+	//double vals[3] = {xs, ys, zs};
+//	Serial.print("x=");
+//	Serial.println(vals[0]);
+	return vals;
 }
--- src/header/gyroscope.h
+++ src/header/gyroscope.h
@@ -4,6 +4,7 @@
 #include "types.h"
 #include <MeGyro.h>
 
+
 class Gyroscope{
 	private:
 		MeGyro sensors[2];
@@ -11,9 +12,14 @@ class Gyroscope{
 		const int iterations;
 
 	public:
+		enum Axe{
+			X=0,
+			Y,
+			Z
+		};
 		Gyroscope(uint8_t ports[2], int max_iter);
 		void reset();
-		double angle();
+		double* angle();
 };
 
 #endif
