35413c6395b962bfedd4092185ffd2ea3d0fbfae
Nicolai Dagestad <nicolai@dagestad.fr>
2018-10-18 18:29:35 +0200

    Path generator done

--- path_generator/generator.cpp
+++ path_generator/generator.cpp
@@ -15,32 +15,116 @@ Generator::~Generator(){
 }
 
 #include <iostream>
+#include <vector>
+
+void orient(Generator::Direction start, Generator::Direction next){
+	using namespace std;
+	switch ( start ){
+		case Generator::HAUT:{
+			switch ( next ){
+				case Generator::HAUT:{
+					break;
+				}
+				case Generator::BAS: {
+					cout << "TOURNER DROITE TOURNER DROITE" << endl;
+					break;
+				}
+				case Generator::DROITE:{
+					cout << "TOURNER DROITE" << endl;
+					break;
+				}
+				case Generator::GAUCHE:{
+					cout << "TOURNER GAUCHE" << endl;
+					break;
+				}
+			}
+			break;
+		}
+		case Generator::BAS:{
+			switch ( next ){
+				case Generator::HAUT:{
+					cout << "TOURNER DROITE TOURNER DROITE" << endl;
+					break;
+				}
+				case Generator::BAS: {
+					break;
+				}
+				case Generator::DROITE:{
+					cout << "TOURNER GAUCHE" << endl;
+					break;
+				}
+				case Generator::GAUCHE:{
+					cout << "TOURNER DROITE" << endl;
+					break;
+				}
+			}
+			break;
+		}
+		case Generator::DROITE:{
+			switch ( next ){
+				case Generator::HAUT:{
+					cout << "TOURNER GAUCHE" << endl;
+					break;
+				}
+				case Generator::BAS: {
+					cout << "TOURNER DROITE" << endl;
+					break;
+				}
+				case Generator::DROITE:{
+					break;
+				}
+				case Generator::GAUCHE:{
+					cout << "TOURNER DROITE TOURNER DROITE" << endl;
+					break;
+				}
+			}
+			break;
+		}
+		case Generator::GAUCHE:{
+			switch ( next ){
+				case Generator::HAUT:{
+					cout << "TOURNER DROITE" << endl;
+					break;
+				}
+				case Generator::BAS: {
+					cout << "TOURNER GAUCHE" << endl;
+					break;
+				}
+				case Generator::DROITE:{
+					cout << "TOURNER DROITE TOURNER DROITE" << endl;
+					break;
+				}
+				case Generator::GAUCHE:{
+					break;
+				}
+			}
+			break;
+		}
+	}
+}
+
+int abs(int a){
+	if ( a < 0 ) return -a;
+	return a;
+}
+
 void Generator::generate(){
 	using namespace std;
 	auto prev=nodes.begin();
+	Direction robot = Direction::HAUT;
+	cout << "1 AVANCER " << endl;
 	for ( auto it=++(nodes.begin()); it != nodes.end(); it++){
-		cerr << "(" << prev->x << ", " << prev->y << ") -> (" << it->x << ", " << it->y << ")"; // ne marche pas bizarrement
-		switch (this->direction(*prev, *it) ){
-		case HAUT:
-			cout << "HAUT" << endl;
-			break;
-		case BAS:
-			cout << "BAS" << endl;
-			break;
-		case GAUCHE:
-			cout << "GAUCHE" << endl;
-			break;
-		case DROITE:
-			cout << "DROITE" << endl;
-			break;
-		default:
-			abort();
-		}
+		cerr << "(" << (int)prev->x << ", " << (int)prev->y << ") -> (" << (int)it->x << ", " << (int)it->y << ")" << endl; // ne marche pas bizarrement
+		Direction suiv = this->direction(*prev, *it);
+		int distance = max(abs(it->x-prev->x), abs(it->y - prev->y));
+		orient(robot, suiv);
+		robot=suiv;
+		cout << "\t" <<  distance << " AVANCER" << endl;
 		prev=it;
 	}
 }
 
-Generator::Direction Generator::direction(::pair<uchar> A, ::pair<uchar> B){
+Generator::Direction Generator::direction(::pair<char> A, ::pair<char> B){
 	int dx = A.x - B.x;	
 	int dy = A.y - B.y;
 	if ( dx > 0 ){
@@ -74,7 +158,7 @@ void Generator::parse_file(){
 			b=atoi(pos2.c_str());
 		cerr << name << "-> a: " <<a << ", b=" << b << endl;
 		nodes.push_back(
-			::pair<uchar>(a,b)
+			::pair<char>(a,b)
 		);
 	}
 
